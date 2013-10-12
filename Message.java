e8ccc67259094e13b9aa113aed578e915e17fb2d
SATHISHK <sathish@ubuntu.(none)>
2013-10-12 01:36:28 -0700

    adding message class

---
+++ Message.java
@@ -0,0 +1,26 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Message here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Message extends Actor
+{
+    /**
+     * Act - do whatever the Message wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+     public Message(String message)
+    {
+        GreenfootImage image = getImage() ;
+        image.scale( 25, 25 ) ; 
+
+        image.drawString(message,5, 15);
+    }
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
